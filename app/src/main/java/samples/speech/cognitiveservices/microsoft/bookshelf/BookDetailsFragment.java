6354fef7d2b415db00d0d1027ec19ec2c397dd00
Curtis Kieser <tuh39868@temple.edu>
2020-04-23 21:55:09 -0400

    Implement working assignment 8

--- app/src/main/java/samples/speech/cognitiveservices/microsoft/bookshelf/BookDetailsFragment.java
+++ app/src/main/java/samples/speech/cognitiveservices/microsoft/bookshelf/BookDetailsFragment.java
@@ -1,47 +1,80 @@
 package samples.speech.cognitiveservices.microsoft.bookshelf;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
 
 public class BookDetailsFragment extends Fragment {
 
-    private static final String ID_KEY = "id";
-    int id = -1;
-    TextView titleTextView;
-    View myLayout;
+    private static final String BOOK_KEY = "book";
+    private Book book;
 
-    public BookDetailsFragment(){}
+    TextView titleTextView, authorTextView;
+    ImageView coverImageView;
 
-    public static BookDetailsFragment newInstance(int id){
+    public BookDetailsFragment() {}
+
+    public static BookDetailsFragment newInstance(Book book) {
         BookDetailsFragment fragment = new BookDetailsFragment();
-        Bundle bundle = new Bundle();
-        bundle.putInt(ID_KEY, id);
-        fragment.setArguments(bundle);
+        Bundle args = new Bundle();
+
+        /*
+         Our Book class implements the Parcelable interface
+         therefore we can place one inside a bundle
+         by using that put() method.
+         */
+        args.putParcelable(BOOK_KEY, book);
+        fragment.setArguments(args);
         return fragment;
     }
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        myLayout = inflater.inflate(R.layout.book_detail_view, container, false);
-        return myLayout;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            book = (Book) getArguments().getParcelable(BOOK_KEY);
+        }
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
-        super.onViewCreated(view, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_book_details, container, false);
 
-        titleTextView = (TextView)view.findViewById(R.id.bookDetailView);
+        titleTextView = v.findViewById(R.id.titleTextView);
+        authorTextView = v.findViewById(R.id.authorTextView);
+        coverImageView = v.findViewById(R.id.coverImageView);
+
+        /*
+        Because this fragment can be created with or without
+        a book to display when attached, we need to make sure
+        we don't try to display a book if one isn't provided
+         */
+        if (book != null)
+            displayBook(book);
+        return v;
     }
 
-    void changeDetails(String title){
-        titleTextView.setText(title);
-
+    /*
+    This method is used both internally and externally (from the activity)
+    to display a book
+     */
+    public void displayBook(Book book) {
+        titleTextView.setText(book.getTitle());
+        authorTextView.setText(book.getAuthor());
+        // Picasso simplifies image loading from the web.
+        // No need to download separately.
+        Picasso.get().load(book.getCoverUrl()).into(coverImageView);
     }
 }
--- app/src/main/java/samples/speech/cognitiveservices/microsoft/bookshelf/BookListFragment.java
+++ app/src/main/java/samples/speech/cognitiveservices/microsoft/bookshelf/BookListFragment.java
@@ -1,114 +1,96 @@
 package samples.speech.cognitiveservices.microsoft.bookshelf;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BookListFragment extends Fragment {
 
-    ListView bookList;
+    private static final String BOOK_LIST_KEY = "booklist";
+    private ArrayList<Book> books;
+    private ListView listView;
 
-    private static final String ID_KEY = "id";
-    int id = -1;
-    public String selectedBook;
+    private BookSelectedInterface parentActivity;
 
-    ChosenBookInterface parentActivity;
+    public BookListFragment() {}
 
-    public BookListFragment(){}
-
-    public static BookListFragment newInstance (int id){
+    public static BookListFragment newInstance(ArrayList<Book> books) {
         BookListFragment fragment = new BookListFragment();
-        Bundle bundle = new Bundle();
-        bundle.putInt(ID_KEY, id);
-        fragment.setArguments(bundle);
+        Bundle args = new Bundle();
+
+        /*
+         A Book implements the Parcelable interface
+         therefore we can place an arraylist of books
+         inside a bundle by using that put() method.
+         */
+        args.putParcelableArrayList(BOOK_LIST_KEY, books);
+        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-
-        Bundle bundle = getArguments();
-
-        if(bundle != null){
-            id = bundle.getInt("id");
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View bookListView = inflater.inflate(R.layout.book_text_view, container, false);
-
-        bookList = bookListView.findViewById(R.id.ListBook);
-        getBooks();
-        return bookListView;
-    }
-
-    public void getBooks(){
-
-        Resources res = getResources();
-        String[] titles = res.getStringArray(R.array.bookTitles);
-        int listSize = titles.length;
-        String[] authors = res.getStringArray(R.array.bookAuthors);
-
-        ArrayList<HashMap<String, String>> bookListArray = new ArrayList<HashMap<String, String>>();
-        HashMap<String,String> hm = new HashMap<String, String>();
-        for(int i=0; i < listSize; i++){
-            hm.put(titles[i], authors[i]);
-            bookListArray.add(hm);
-        }
-        //bookListArray.add(hm);
-
-        ArrayAdapter<HashMap<String, String>> bookListAdapter =
-                new ArrayAdapter<HashMap<String, String>>(getActivity(),
-                        android.R.layout.simple_list_item_1, bookListArray);
-
-        bookList.setAdapter(bookListAdapter);
-        bookList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.v("item", (String) parent.getItemAtPosition(position));
-                selectedBook = (String) parent.getItemAtPosition(position);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-    }
-
-    interface ChosenBookInterface{
-        public void sendBook(String thisBook);
-    }
-
-    @Override
-    public void onAttach(Context context){
+    public void onAttach(Context context) {
         super.onAttach(context);
 
-        try{
-            parentActivity = (ChosenBookInterface) getActivity();
-        }catch (ClassCastException e) {
-            throw new ClassCastException("Error retrieving data. Please try again.");
+        /*
+         This fragment needs to communicate with its parent activity
+         so we verify that the activity implemented our known interface
+         */
+        if (context instanceof BookSelectedInterface) {
+            parentActivity = (BookSelectedInterface) context;
+        } else {
+            throw new RuntimeException("Please implement the required interface(s)");
         }
     }
 
-    public int getFragmentId(){return id;}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        books = new ArrayList<Book>();
+        if (getArguments() != null) {
+            books.addAll((ArrayList) getArguments().getParcelableArrayList(BOOK_LIST_KEY));
+        }
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        listView = (ListView) inflater.inflate(R.layout.fragment_book_list, container, false);
+
+        listView.setAdapter(new BooksAdapter(getContext(), books));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                parentActivity.bookSelected(position);
+            }
+        });
+
+        return listView;
+    }
+
+    public void updateBooksDisplay(ArrayList<Book> books) {
+        this.books.clear();
+        this.books.addAll(books);
+        ((BaseAdapter) listView.getAdapter()).notifyDataSetChanged();
+    }
+
+    /*
+    Interface for communicating with attached activity
+     */
+    interface BookSelectedInterface {
+        void bookSelected(int index);
+    }
 }
-
-
-
